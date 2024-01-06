f57ba457a0a54ef9da4c1a9f3e9dbf06682f74f6
Daniel Diaz <ddiaz.enterprise@gmail.com>
2024-01-05 22:34:46 -0600

    upload now returns file data with id

--- src/controllers/files.ts
+++ src/controllers/files.ts
@@ -9,46 +9,58 @@ import { log } from 'console';
 const router = Router();
 const storagePath = process.env.STORAGE_PATH;
 // Set storage engine for multer
-const storage = multer.diskStorage({
+const storage = (id: string) => multer.diskStorage({
     destination: storagePath,
     filename: async function (req, file, cb) {
-        const id = v4();
         const extension = path.extname(file.originalname);
-        const record = {
-            originalname: file.originalname,
-            date: Date.now(),
-            filename: `${id}${extension}`,
-            mimetype: file.mimetype,
-            extension,
-            id,
-            meta: file
-        }
-        const query = `
-        INSERT INTO public.files (id, date, extension, originalname, mimetype)
-        VALUES ($1, $2, $3, $4, $5)
-        `;
-
-        const res = await pool.query(query, [id, new Date(), extension, record.originalname, record.mimetype]);
-        log("NEW RECORD: ", record.filename)
-        log('Insert successful:', res.rowCount);
-        cb(null, record.filename);
+        cb(null, `${id}${extension}`);
     }
-});
+})
+
 
 // Init upload
-const upload = multer({
-    storage: storage
-}).single('myFile'); // 'myFile' is the field name in the form
+const upload = (id: string) => multer({
+    storage: storage(id),
+}).single('myFile')// 'myFile' is the field name in the form
 
 // Upload route
-router.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            res.send('Error uploading file.');
-        } else {
-            res.send('File uploaded successfully.');
-        }
-    });
+router.post('/upload', (req, res, next) => {
+    const id = v4();
+    // @ts-ignore
+    req.id = id;
+    upload(id)(req, res, () => {
+        next();
+    })
+}, async (req, res) => {
+    // @ts-ignore
+    const id = req.id;
+    const file = req.file;
+    console.log("file", file)
+    console.log("id", id)
+    if (!file) {
+        return res.status(400).send('Please upload a file.');
+    }
+
+    const extension = path.extname(file.originalname);
+    const record = {
+        originalname: file.originalname,
+        date: Date.now(),
+        filename: `${id}${extension}`,
+        mimetype: file.mimetype,
+        extension,
+        id,
+        meta: file,
+    }
+    const query = `
+    INSERT INTO public.files (id, date, extension, originalname, mimetype)
+    VALUES ($1, $2, $3, $4, $5)
+    `;
+
+    const result = await pool.query(query, [id, new Date(), extension, record.originalname, record.mimetype]);
+    log("NEW RECORD: ", record.filename)
+    log('Insert successful:', result.rowCount);
+
+    res.send(record);
 });
 
 // Download route
@@ -60,13 +72,7 @@ router.get('/download/:filename', (req, res) => {
         return;
     }
     const file = path.join(storagePath, filename);
-    const fileExists = existsSync(file);
-    if (!fileExists) {
-        const htmlFile = path.join(__dirname, '../html/404.html');
-        res.sendFile(htmlFile);
-    } else {
-        res.download(file);
-    }
+    res.download(file);
 });
 
 router.get('/files', async (req, res) => {
